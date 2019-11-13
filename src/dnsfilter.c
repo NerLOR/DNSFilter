5d4c9e23ce786f11a2528e8de8293a0f771d4601
Lorenz Stechauner <lorenz.stechauner@gmail.com>
2019-11-13 19:16:18 +0100

    Added dnsfilter.c

---
+++ src/dnsfilter.c
@@ -0,0 +1,16 @@
+/**************************
+ * DNSFilter
+ * Lorenz Stechauner, 2019
+ **************************/
+
+
+static int __init dnsfilter_init(void) {
+
+}
+
+static void __exit dnsfilter_exit(void) {
+
+}
+
+module_init(dnsfilter_init);
+module_exit(dnsfilter_exit);
